d1658dc0848950ce0cc604dfe5a41d6faff38152
Charles Wahome <thewahome.cw@gmail.com>
2020-09-18 16:15:20 +0300

    test that sample body is a valid json string

--- tests/samples.spec.js
+++ tests/samples.spec.js
@@ -6,3 +6,25 @@ describe('sample query should', function () {
     expect(sampleQueries.length).toBeGreaterThan(1);
   });
 });
+
+describe('sample queries postbody', function () {
+  const sampleQueries = samples.SampleQueries;
+  const queries = sampleQueries.filter(sample => sample.postBody);
+  for (const query of queries) {
+    it(`${query.humanName}: should be a valid json string`, function () {
+      let isValidJson = true;
+      if (query.headers) {
+        const contentTypeHeaders = query.headers.find(header => header.name.toLowerCase().includes('content-type'));
+        // xml content will not pass test
+        if (contentTypeHeaders && !contentTypeHeaders.value.includes('xml')) {
+          try {
+            JSON.parse(query.postBody);
+          } catch (error) {
+            isValidJson = false;
+          }
+        }
+      }
+      expect(isValidJson).toEqual(true);
+    });
+  }
+});
