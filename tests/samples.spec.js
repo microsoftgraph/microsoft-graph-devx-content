a328fbf9673446d5b3615be1ce4164a1d51bdcbf
Charles Wahome <thewahome.cw@gmail.com>
2020-09-21 11:15:18 +0300

    combine queries into one loop

--- tests/samples.spec.js
+++ tests/samples.spec.js
@@ -7,24 +7,36 @@ describe('sample query should', function () {
   });
 });
 
-describe('sample queries postbody', function () {
-  const sampleQueries = samples.SampleQueries;
-  const queries = sampleQueries.filter(sample => sample.postBody);
-  for (const query of queries) {
-    it(`${query.humanName}: should be a valid json string`, function () {
-      let isValidJson = true;
-      if (query.headers) {
-        const contentTypeHeaders = query.headers.find(header => header.name.toLowerCase().includes('content-type'));
-        // xml content will not pass test
-        if (contentTypeHeaders && !contentTypeHeaders.value.includes('xml')) {
-          try {
-            JSON.parse(query.postBody);
-          } catch (error) {
-            isValidJson = false;
+const sampleQueries = samples.SampleQueries;
+for (const query of sampleQueries) {
+  describe(`${query.humanName}:`, function () {
+    it('doclink should exist', function () {
+      const docLinkExists = !!query.docLink;
+      expect(docLinkExists).toEqual(true);
+    });
+
+    it('humanName first word should be lower cased', function () {
+      const firstWord = query.humanName.split(' ')[0];
+      const lowerCasedHumanName = firstWord.toLowerCase();
+      expect(lowerCasedHumanName).toEqual(firstWord);
+    });
+
+    if (query.postBody) {
+      it(`postbody should be a valid json string`, function () {
+        let isValidJson = true;
+        if (query.headers) {
+          const contentTypeHeaders = query.headers.find(header => header.name.toLowerCase().includes('content-type'));
+          // xml content will not pass test
+          if (contentTypeHeaders && !contentTypeHeaders.value.includes('xml')) {
+            try {
+              JSON.parse(query.postBody);
+            } catch (error) {
+              isValidJson = false;
+            }
           }
         }
-      }
-      expect(isValidJson).toEqual(true);
-    });
-  }
-});
+        expect(isValidJson).toEqual(true);
+      });
+    }
+  });
+}
