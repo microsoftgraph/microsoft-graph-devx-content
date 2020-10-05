f1e53e514b32cd61c19f066a13e9ee7d87f86400
Charles Wahome <thewahome.cw@gmail.com>
2020-10-05 17:46:08 +0300

    Task: add tests (#64)
    * initialise test runs
    
    * add node_modules to gitignore
    
    * add sample test for more than one sample
    
    * test that sample body is a valid json string
    
    * combine queries into one loop
    
    * add check for id exists
    
    * add github actions
    
    * rename job title
    
    * add shema to validate json schema
    
    * add checkout to step
    
    * remove extra properties from schema
    
    * remove tests covered by schema check
    
    * id value should be uuid
    
    * doclink should be a uri
    
    * id should be unique
    
    * fix length check for sample id
    
    * add enum to method validator
    
    * method validation should fail
    
    * add additional properties to check enum
    
    * add methods to method-enum

---
+++ tests/samples.spec.js
@@ -0,0 +1,36 @@
+const samples = require('../sample-queries/sample-queries.json');
+
+const sampleQueries = samples.SampleQueries;
+for (const query of sampleQueries) {
+  describe(`${query.humanName}:`, function () {
+
+    it('id should be unique', function () {
+      const count = sampleQueries.filter(samp => samp.id === query.id).length;
+      expect(count).toEqual(1);
+    });
+
+    it('humanName first word should be lower cased', function () {
+      const firstWord = query.humanName.split(' ')[0];
+      const lowerCasedHumanName = firstWord.toLowerCase();
+      expect(lowerCasedHumanName).toEqual(firstWord);
+    });
+
+    if (query.postBody) {
+      it(`postbody should be a valid json string`, function () {
+        let isValidJson = true;
+        if (query.headers) {
+          const contentTypeHeaders = query.headers.find(header => header.name.toLowerCase().includes('content-type'));
+          // xml content will not pass test
+          if (contentTypeHeaders && !contentTypeHeaders.value.includes('xml')) {
+            try {
+              JSON.parse(query.postBody);
+            } catch (error) {
+              isValidJson = false;
+            }
+          }
+        }
+        expect(isValidJson).toEqual(true);
+      });
+    }
+  });
+}
